e583874cd9961427bdc2859ee47e097252e6c3a5
Abdelrahman Deghedy <68364202+AbdelrahmanDeghedy@users.noreply.github.com>
2020-08-11 15:32:57 +0200

    Add files via upload

---
+++ Week 2 Assignment/1 Checking Consistency of CS Curriculum/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+vector <vector <int>> adj (1009);
+vector <int> finish (1009, -1);
+vector <int> start (1009, -1);
+int timer = 1;
+bool cycle = 0;
+
+void DFS_Cycle (int x)
+{
+    start [x] = timer ++;
+    for (int i = 0; i < (int) adj[x].size(); i++)
+    {
+        int child = adj[x][i];
+        if (start[child] == -1)
+            DFS_Cycle (child);
+        else
+        {
+            if (finish[child] == -1)
+                cycle = 1;
+        }
+    }
+    finish[x] = timer++;
+}
+
+int main()
+{
+    int n, m, x, y;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> x >> y;
+        adj[x].push_back (y);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (start[i] == -1)
+            DFS_Cycle (i);
+    }
+    cout << cycle;
+
+    return 0;
+}
